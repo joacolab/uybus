08d5872af303fc142d48dc6cac4041d092c5e71f
luisjoaquinsuarez123@gmail.com <luisjoaquinsuarez123@gmail.com>
2020-10-03 21:52:01 -0300

    ready base structure

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,15 +1,26 @@
-﻿using System;
+﻿using DataAccessLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Console
+namespace Console1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (uybusEntities db = new uybusEntities()) {
+                Parametro p = new Parametro();
+                p.Valor = 5;
+                db.Parametro.Add(p);
+                db.SaveChanges();
+            }
+            Console.Read();
         }
     }
 }
+
+
+
