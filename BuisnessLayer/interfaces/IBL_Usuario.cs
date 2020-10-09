7317d3572eaba7ce1771b840f2a6b1fb61ea327e
alfa jor 144 <karloxx09@gmail.com>
2020-10-09 19:54:55 -0300

    prontas firmas de usuario

--- BuisnessLayer/interfaces/IBL_Usuario.cs
+++ BuisnessLayer/interfaces/IBL_Usuario.cs
@@ -9,5 +9,6 @@ namespace BuisnessLayer.interfaces
     public interface IBL_Usuario
     {
         void comprarPasaje(int idViaje, int idUsuario, int idParadaOrigen, int idParadaDestino, System.DateTime fechaViaje, string tipoDoc, string documento, int idParametro, int asiento);
+        void proximoVehiculo(int idUsuario, int idParada);
     }
 }
